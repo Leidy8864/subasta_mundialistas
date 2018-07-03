2a697e9adbb550ac7284799c71456b1a547ee13c
Leidy8864 <33355476+Leidy8864@users.noreply.github.com>
2018-07-03 15:18:46 -0500

    prueba con original

--- config/auth.js
+++ config/auth.js
@@ -8,6 +8,7 @@ module.exports = {
 	'googleAuth' : {
 		'clientID': '211235760800-hk0j8stphn6h16masg5rg56j6omab8e8.apps.googleusercontent.com',
 		'clientSecret': 'HFkFjW0Z3bX52AWHcW33WEkb',
-		'callbackURL': 'https://subasta-mundialista.herokuapp.com/auth/google/callback'
+		'callbackURL': 'http://localhost:3000/auth/google/callback'
+        //'callbackURL': 'https://subasta-mundialista.herokuapp.com/auth/google/callback'
 	}
 }
