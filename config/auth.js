e768130496a9316211140a6efccf89fe64326b8a
Leidy8864 <33355476+Leidy8864@users.noreply.github.com>
2018-07-03 16:16:44 -0500

    funciona login

--- config/auth.js
+++ config/auth.js
@@ -6,9 +6,11 @@ module.exports = {
 	},
 
 	'googleAuth' : {
-		'clientID': '211235760800-hk0j8stphn6h16masg5rg56j6omab8e8.apps.googleusercontent.com',
-		'clientSecret': 'HFkFjW0Z3bX52AWHcW33WEkb',
-		'callbackURL': 'http://localhost:3000/auth/google/callback'
-        //'callbackURL': 'https://subasta-mundialista.herokuapp.com/auth/google/callback'
+		//'clientID': '211235760800-hk0j8stphn6h16masg5rg56j6omab8e8.apps.googleusercontent.com',
+		'clientID': '603648208156-bt6geneu0b0j274r3405poemfjn3mg8f.apps.googleusercontent.com',
+		//'clientSecret': 'HFkFjW0Z3bX52AWHcW33WEkb',
+		'clientSecret': 'jch_7s3WzRVx2TBmr2hgstsw',
+		'callbackURL': 'https://subasta-mundialista.herokuapp.com/auth/google/callback'
+		//'callbackURL': 'http://localhost:3000/auth/google/callback'
 	}
 }
